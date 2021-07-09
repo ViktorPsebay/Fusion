c8cf2fb2b9c0f4a1ef9bf9b20b25cf98b36dce7a
Daniil Salamatin <salamatindanya21@gmail.com>
2021-07-09 17:41:04 +0300

    finish an auto ship placement

--- seaBattle.js
+++ seaBattle.js
@@ -1,12 +1,8 @@
-function CellsPosition(row, column) {
-  this.row = row;
-  this.column = column;
-}
-
 function Field() {
   this.mode = "wait";
   this.available = [];
   this.chosen = [];
+  this.shoted = [];
   this.ships = new Array(4).fill(0);
 }
 
@@ -15,23 +11,21 @@ function Cell(row, column) {
   this.column = column;
   this.busy = false;
   this.shot = false;
-  this.available = false;
 }
 
-const playerCell = [];
+const playerCells = [];
 for (let i = 0; i < 10; ++i) {
-  playerCell.push(new Array(10));
-  for (let j = 0; j < 10; ++j) playerCell[i][j] = new Cell(i, j);
+  playerCells.push(new Array(10));
+  for (let j = 0; j < 10; ++j) playerCells[i][j] = new Cell(i, j);
 }
 
-const computerCell = [];
+const computerCells = [];
 for (let i = 0; i < 10; ++i) {
-  computerCell.push(new Array(10));
-  for (let j = 0; j < 10; ++j) computerCell[i][j] = new Cell(i, j);
+  computerCells.push(new Array(10));
+  for (let j = 0; j < 10; ++j) computerCells[i][j] = new Cell(i, j);
 }
 
 const playerHandler = document.querySelector(".player");
-
 for (let i = 0; i < 10; ++i) {
   for (let j = 0; j < 10; ++j) {
     let cell = document.createElement("div");
@@ -53,7 +47,10 @@ for (let i = 0; i < 10; ++i) {
   }
 }
 
+const NumberOfShips = [4, 3, 2, 1];
+
 const playerField = new Field();
+const computerField = new Field();
 
 const playerGrid = document.querySelectorAll(".player__cell");
 const computerGrid = document.querySelectorAll(".computer__cell");
@@ -61,59 +58,71 @@ const computerGrid = document.querySelectorAll(".computer__cell");
 const setShipButton = document.querySelector(".menu__setShip");
 const delShipButton = document.querySelector(".menu__delShip");
 const newGameButton = document.querySelector(".menu__newGame");
-const autoAlignment = document.querySelector(".menu__autoAlignment");
+const autoAlignmentPlayer = document.querySelector(".menu__autoAlignment");
+const startBattleButton = document.querySelector(".menu__startBattle");
 
-autoAlignment.addEventListener("click", () => {
-  let row = Math.floor(Math.random() * computerCell.length);
-  let column = Math.floor(Math.random() * computerCell.length);
+autoAlignmentPlayer.addEventListener("click", () => {
+  playerGrid[autoAlignment(playerField, playerCells, playerGrid)].dispatchEvent(
+    new Event("click", { bubbles: true })
+  );
+
+  while (!array_compare(playerField.ships, NumberOfShips)) {
+    autoAlignmentPlayer.dispatchEvent(new Event("click"));
+  }
 });
 
 newGameButton.addEventListener("click", () => {
   location.reload();
 });
 
+startBattleButton.addEventListener("click", () => {
+  autoAlignmentOfComputer(computerField, computerCells, computerGrid);
+  playerField.mode = "battle";
+  computerField.mode = "battle";
+});
+
 setShipButton.addEventListener("click", () => {
-  if (!playerField.chosen.length) return;
-  if (
-    playerField.ships[playerField.chosen.length - 1] >=
-    [4, 3, 2, 1][playerField.chosen.length - 1]
-  )
-    return;
-  let chosen;
-  playerField.ships[playerField.chosen.length - 1]++;
-  while ((chosen = playerField.chosen.pop())) {
-    chosen.busy = true;
-    playerGrid[chosen.row * 10 + chosen.column].style.backgroundColor = "black";
-    removeAvailableCells(playerField);
-    playerField.mode = "wait";
-  }
+  setShip(playerField, playerGrid);
 });
 
 delShipButton.addEventListener("click", () => {
-  if (!playerField.chosen.length) return;
-  let chosen;
-  while ((chosen = playerField.chosen.pop())) {
-    playerGrid[chosen.row * 10 + chosen.column].style.backgroundColor = "white";
-    removeAvailableCells(playerField);
-    playerField.mode = "wait";
+  delShip(playerField, playerGrid);
+});
+
+computerHandler.addEventListener("click", (event) => {
+  if (computerField.mode === "battle") {
+    let row = +event.target.dataset.row;
+    let column = +event.target.dataset.column;
+    shot(row, column, computerField, computerCells, computerGrid);
+    return;
   }
+  if (!event.target.classList.contains("computer__cell")) return;
+  if (array_compare(computerField.ships, NumberOfShips)) return;
+  let row = +event.target.dataset.row;
+  let column = +event.target.dataset.column;
+  if (!checkAvailability(row, column, computerField, computerCells)) return;
+
+  event.target.style.backgroundColor = "red";
+  computerField.chosen.push(computerCells[row][column]);
+  setAvailableCell(row, column, computerField, computerGrid, computerCells);
+  computerField.mode = "arrangement";
 });
 
 playerHandler.addEventListener("click", (event) => {
   if (!event.target.classList.contains("player__cell")) return;
-  if (array_compare(playerField.ships, [4, 3, 2, 1])) return;
+  if (array_compare(playerField.ships, NumberOfShips)) return;
   let row = +event.target.dataset.row;
   let column = +event.target.dataset.column;
-  if (!checkAvailability(row, column, playerField)) return;
+  if (!checkAvailability(row, column, playerField, playerCells)) return;
 
   event.target.style.backgroundColor = "red";
-  playerField.chosen.push(playerCell[row][column]);
-  setAvailableCell(row, column, playerField);
+  playerField.chosen.push(playerCells[row][column]);
+  setAvailableCell(row, column, playerField, playerGrid, playerCells);
   playerField.mode = "arrangement";
 });
 
-function setAvailableCell(row, column, field) {
-  removeAvailableCells(field);
+function setAvailableCell(row, column, field, grid, cells) {
+  removeAvailableCells(field, grid);
   if (field.chosen.length > 3) return;
   else if (field.chosen.length === 3 && field.ships[3] > 0) return;
   else if (
@@ -136,9 +145,9 @@ function setAvailableCell(row, column, field) {
       [1, 0],
       [-1, -1],
     ];
-    if (column >= 0 && checkNeighbour(row, column, neighbours)) {
-      field.available.push(playerCell[row][column]);
-      playerGrid[row * 10 + column].style.backgroundColor = "green";
+    if (column >= 0 && checkNeighbour(row, column, cells, neighbours)) {
+      field.available.push(cells[row][column]);
+      grid[row * 10 + column].style.backgroundColor = "green";
     }
     column += 2;
     neighbours = [
@@ -146,9 +155,12 @@ function setAvailableCell(row, column, field) {
       [1, 0],
       [-1, 1],
     ];
-    if (column < playerCell.length && checkNeighbour(row, column, neighbours)) {
-      field.available.push(playerCell[row][column]);
-      playerGrid[row * 10 + column].style.backgroundColor = "green";
+    if (
+      column < cells.length &&
+      checkNeighbour(row, column, cells, neighbours)
+    ) {
+      field.available.push(cells[row][column]);
+      grid[row * 10 + column].style.backgroundColor = "green";
     }
     column--;
     row--;
@@ -157,9 +169,9 @@ function setAvailableCell(row, column, field) {
       [0, 1],
       [-1, -1],
     ];
-    if (row >= 0 && checkNeighbour(row, column, neighbours)) {
-      field.available.push(playerCell[row][column]);
-      playerGrid[row * 10 + column].style.backgroundColor = "green";
+    if (row >= 0 && checkNeighbour(row, column, cells, neighbours)) {
+      field.available.push(cells[row][column]);
+      grid[row * 10 + column].style.backgroundColor = "green";
     }
     row += 2;
     neighbours = [
@@ -167,9 +179,9 @@ function setAvailableCell(row, column, field) {
       [0, 1],
       [1, -1],
     ];
-    if (row < playerCell.length && checkNeighbour(row, column, neighbours)) {
-      field.available.push(playerCell[row][column]);
-      playerGrid[row * 10 + column].style.backgroundColor = "green";
+    if (row < cells.length && checkNeighbour(row, column, cells, neighbours)) {
+      field.available.push(cells[row][column]);
+      grid[row * 10 + column].style.backgroundColor = "green";
     }
   } else if (field.mode === "arrangement") {
     if (field.chosen[0].row === row) {
@@ -180,9 +192,9 @@ function setAvailableCell(row, column, field) {
         [1, 0],
         [-1, -1],
       ];
-      if (column >= 0 && checkNeighbour(row, column, neighbours)) {
-        field.available.push(playerCell[row][column]);
-        playerGrid[row * 10 + column].style.backgroundColor = "green";
+      if (column >= 0 && checkNeighbour(row, column, cells, neighbours)) {
+        field.available.push(cells[row][column]);
+        grid[row * 10 + column].style.backgroundColor = "green";
       }
       column = Math.max(...columns) + 1;
       neighbours = [
@@ -191,11 +203,11 @@ function setAvailableCell(row, column, field) {
         [-1, 1],
       ];
       if (
-        column < playerCell.length &&
-        checkNeighbour(row, column, neighbours)
+        column < cells.length &&
+        checkNeighbour(row, column, cells, neighbours)
       ) {
-        field.available.push(playerCell[row][column]);
-        playerGrid[row * 10 + column].style.backgroundColor = "green";
+        field.available.push(cells[row][column]);
+        grid[row * 10 + column].style.backgroundColor = "green";
       }
     } else if (field.chosen[0].column === column) {
       let rows = field.chosen.map((item) => item.row);
@@ -205,9 +217,9 @@ function setAvailableCell(row, column, field) {
         [0, 1],
         [-1, -1],
       ];
-      if (row >= 0 && checkNeighbour(row, column, neighbours)) {
-        field.available.push(playerCell[row][column]);
-        playerGrid[row * 10 + column].style.backgroundColor = "green";
+      if (row >= 0 && checkNeighbour(row, column, cells, neighbours)) {
+        field.available.push(cells[row][column]);
+        grid[row * 10 + column].style.backgroundColor = "green";
       }
       row = Math.max(...rows) + 1;
       neighbours = [
@@ -215,63 +227,62 @@ function setAvailableCell(row, column, field) {
         [0, 1],
         [1, -1],
       ];
-      if (row < playerCell.length && checkNeighbour(row, column, neighbours)) {
-        field.available.push(playerCell[row][column]);
-        playerGrid[row * 10 + column].style.backgroundColor = "green";
+      if (
+        row < cells.length &&
+        checkNeighbour(row, column, cells, neighbours)
+      ) {
+        field.available.push(cells[row][column]);
+        grid[row * 10 + column].style.backgroundColor = "green";
       }
     }
   }
 }
 
-function checkAvailability(row, column, field) {
+function checkAvailability(row, column, field, cells) {
+  const neighbours = [
+    [0, -1],
+    [0, -1],
+    [1, 0],
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
   if (field.mode === "wait") {
-    if (playerCell[row][column].busy) return false;
-    const neighbours = [
-      [0, -1],
-      [0, -1],
-      [1, 0],
-      [1, 0],
-      [0, 1],
-      [0, 1],
-      [-1, 0],
-      [0, -1],
-    ];
-    if (checkNeighbour(row, column, neighbours)) return true;
+    if (cells[row][column].busy) return false;
+
+    if (checkNeighbour(row, column, cells, neighbours)) return true;
 
     return false;
   }
   if (field.mode === "arrangement") {
-    if (field.available.includes(playerCell[row][column])) return true;
+    if (field.available.includes(cells[row][column])) return true;
     return false;
   }
   if (field.mode === "battle") return false;
 }
 
-function checkNeighbour(row, column, neighbours) {
+function checkNeighbour(row, column, cells, neighbours) {
   let change;
   while ((change = neighbours.pop())) {
     row += change[0];
     column += change[1];
-    if (
-      row < 0 ||
-      column < 0 ||
-      row >= playerCell.length ||
-      column >= playerCell.length
-    )
+    if (row < 0 || column < 0 || row >= cells.length || column >= cells.length)
       continue;
-    if (playerCell[row][column].busy) return false;
+    if (cells[row][column].busy) return false;
   }
   return true;
 }
 
-function removeAvailableCells(field) {
+function removeAvailableCells(field, grid) {
   let availableCell;
   while ((availableCell = field.available.pop())) {
     if (
-      playerGrid[availableCell.row * 10 + availableCell.column].style
+      grid[availableCell.row * 10 + availableCell.column].style
         .backgroundColor === "green"
     )
-      playerGrid[
+      grid[
         availableCell.row * 10 + availableCell.column
       ].style.backgroundColor = "white";
   }
@@ -285,6 +296,166 @@ function array_compare(a, b) {
   return true;
 }
 
-for (let cell of playerGrid) {
-  console.log(cell.dataset.row);
+function autoAlignmentOfComputer(field, cells, grid) {
+  grid[autoAlignment(field, cells, grid)].dispatchEvent(
+    new Event("click", { bubbles: true })
+  );
+
+  while (!array_compare(field.ships, NumberOfShips)) {
+    autoAlignmentOfComputer(field, cells, grid);
+  }
+}
+
+function autoAlignment(field, cells, grid) {
+  let row;
+  let column;
+  let choiceFromAvailable;
+  const neighbours = [
+    [0, -1],
+    [0, -1],
+    [1, 0],
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  if (!field.available.length) {
+    if (field.chosen.length) setShip(field, grid);
+    do {
+      row = Math.floor(Math.random() * cells.length);
+      column = Math.floor(Math.random() * cells.length);
+    } while (
+      cells[row][column].busy ||
+      !checkNeighbour(row, column, cells, neighbours)
+    );
+  } else {
+    choiceFromAvailable = Math.floor(Math.random() * field.available.length);
+    row = field.available[choiceFromAvailable].row;
+    column = field.available[choiceFromAvailable].column;
+  }
+  return row * 10 + column;
+}
+
+function setShip(field, grid) {
+  if (!field.chosen.length) return;
+  if (
+    field.ships[field.chosen.length - 1] >=
+    NumberOfShips[field.chosen.length - 1]
+  ) {
+    delShipButton.dispatchEvent(new Event("click"));
+    return;
+  }
+  let chosen;
+  field.ships[field.chosen.length - 1]++;
+  while ((chosen = field.chosen.pop())) {
+    chosen.busy = true;
+    grid[chosen.row * 10 + chosen.column].style.backgroundColor = "black";
+    removeAvailableCells(field, grid);
+    field.mode = "wait";
+  }
+}
+
+function delShip(field, grid) {
+  if (!field.chosen.length) return;
+  let chosen;
+  while ((chosen = field.chosen.pop())) {
+    grid[chosen.row * 10 + chosen.column].style.backgroundColor = "white";
+    removeAvailableCells(field, grid);
+    field.mode = "wait";
+  }
+}
+
+function shot(row, column, field, cells, grid) {
+  if (cells[row][column].busy) {
+    cells[row][column].shot = true;
+    //grid[row * 10 + column].style.backgroundColor = "orange";explosion
+    grid[row * 10 + column].classList.add("explosion");
+    if (!isAlive(row, column, cells)) {
+      let neighbours = [
+        [0, -1],
+        [0, -1],
+        [1, 0],
+        [1, 0],
+        [0, 1],
+        [0, 1],
+        [-1, 0],
+        [0, -1],
+      ];
+      let change;
+      while ((change = neighbours.pop())) {
+        row += change[0];
+        column += change[1];
+        if (
+          row < 0 ||
+          row >= cells.length ||
+          column < 0 ||
+          column >= cells.length
+        )
+          continue;
+        shot(row, column, field, cells, grid);
+      }
+    }
+  } else {
+    //grid[row * 10 + column].style.backgroundColor = "pink";
+
+    let point = document.createElement("div");
+    point.classList.add("point");
+
+    grid[row * 10 + column].append(point);
+    //cells[row][column].shot = true;
+  }
+}
+
+function isAlive(row, column, cells) {
+  // const neighbours = [
+  //   [-1, 1],
+  //   [1, 1],
+  //   [1, -1],
+  //   [-1, 0],
+  // ];
+  // if (!checkNeighbour(row, column, cells, neighbours)) return true;
+  let count = 0;
+  const ship = [cells[row][column]];
+  const neighbours = [
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+    [-1, 0],
+  ];
+  let change;
+  while ((change = neighbours.pop())) {
+    row += change[0];
+    column += change[1];
+    if (row < 0 || row >= cells.length || column < 0 || column >= cells.length)
+      continue;
+    if (cells[row][column].busy) {
+      count++;
+      if (!cells.shot) return true;
+      break;
+    }
+  }
+  shipFormation(row, column, cells, ship);
+}
+
+function shipFormation(row, column, cells, ship) {
+  if (ship[0].row === row) {
+    let minColumn;
+    let maxColumn;
+    if (column > ship[0].column) {
+      ship.push(cells[row][column]);
+      minColumn = ship[0].column;
+      maxColumn = column;
+    } else {
+      minColumn = column;
+      maxColumn = ship[ship.length - 1];
+      ship.shift(cells[row][column]);
+    }
+    while (cells[row][--minColumn].busy) {
+      //if (cells[row][minColumn].shot)
+    }
+  } else {
+    if (row > ship[0].row) ship.push(cells[row][column]);
+    else ship.shift(cells[row][column]);
+  }
 }
